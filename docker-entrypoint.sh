fecac8352cb2de72d331873bdde887e62c3ccf74
Ory Band <oryband@gmail.com>
2016-03-28 17:18:12 +0300

    remove cat-ing cert files to /tmp/combined.pem, causing permission denied
    deals with docker-library/rabbitmq#66 in a hacky way

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -128,10 +128,6 @@ if [ "$1" = 'rabbitmq-server' ]; then
 	fi
 
 	if [ "$ssl" ]; then
-		# Create combined cert
-		cat "$RABBITMQ_SSL_CERT_FILE" "$RABBITMQ_SSL_KEY_FILE" > /tmp/combined.pem
-		chmod 0400 /tmp/combined.pem
-
 		# More ENV vars for make clustering happiness
 		# we don't handle clustering in this script, but these args should ensure
 		# clustered SSL-enabled members will talk nicely
